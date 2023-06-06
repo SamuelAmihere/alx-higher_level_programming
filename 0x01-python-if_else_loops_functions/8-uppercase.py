6061b44e88b3807c1f80a803599874aaa2471d52
SamuelAmihere <amisam2000@gmail.com>
2023-06-06 09:45:57 -0700

    TO uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i, j in zip(range(97, 123), range(65, 91)):
-        if i == 122:
-            print("{}".format(j))
-        if chr(i) in str or chr(j) in str:
-            print("{}".format(j), end="")
+    for ch in str:
+        num = ord(ch)
+        if (num - 32) in range(65, 91):
+            ch = chr(num - 32)
+        elif (_ord + 32) in range(97, 123):
+            ch = chr(num)
+
+        print("{}".format(ch))
+    print("")
