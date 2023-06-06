08a9636c0c034c2484931a376c5fcb44999555fc
SamuelAmihere <amisam2000@gmail.com>
2023-06-06 07:34:09 -0700

    integers - 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02}", end=", ")
+    print("{:02}".format(i), end=", ")
     if i == 99:
-        print(f"{i:2d}")
+        print("{:02}".format(i))
