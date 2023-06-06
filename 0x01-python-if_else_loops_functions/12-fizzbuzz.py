5932519c868231a7e8601d769e655bd1fb167479
SamuelAmihere <amisam2000@gmail.com>
2023-06-06 10:44:51 -0700

    correct err

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i == 100:
-            print("{}".format("Buzz"))
+            print("{} ".format("Buzz"))
             break
         if i % 3 == 0 and i % 5 == 0:
             print("{} ".format("FizzBuzz"), end="")
