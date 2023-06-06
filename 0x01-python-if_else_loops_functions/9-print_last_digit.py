08fab69d5987cee568acc419c56df72bab18f384
SamuelAmihere <amisam2000@gmail.com>
2023-06-06 10:08:07 -0700

    remove newline

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         last_dig = -number % 10
     else:
         last_dig = number % 10
-    print("{}".format(last_dig))
+    print("{}".format(last_dig), end="")
     return (last_dig)
