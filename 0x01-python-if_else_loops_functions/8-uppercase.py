43f9f108e7be23052a4fd6ea2d8e1a2df8f5b9c5
SamuelAmihere <amisam2000@gmail.com>
2023-06-06 09:50:36 -0700

    correct var name error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
         num = ord(ch)
         if (num - 32) in range(65, 91):
             ch = chr(num - 32)
-        elif (_ord + 32) in range(97, 123):
+        elif (num + 32) in range(97, 123):
             ch = chr(num)
 
         print("{}".format(ch), end="")
