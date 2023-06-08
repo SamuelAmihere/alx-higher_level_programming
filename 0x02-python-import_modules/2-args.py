38c4dc65d3e6142b6f1dbb3977a708d050e01e37
SamuelAmihere <amisam2000@gmail.com>
2023-06-08 03:31:55 -0700

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    ac = len(sys.argv) - 1
+
+    header ="{} argument{}:".format(
+            ac,
+            "." if ac == 0  else "s:" if ac != 1 else ":")
+
+    print("{}".format(header))
+
+    if ac > 0:
+        av = sys.argv[1:]
+        for n, arg in enumerate(av):
+            print("{}: {}".format(n + 1, av[n]))
+
