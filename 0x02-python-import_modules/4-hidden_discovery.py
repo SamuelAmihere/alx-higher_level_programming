89aa1de42cceae843ba9169f9f88b180dd60d042
SamuelAmihere <amisam2000@gmail.com>
2023-06-08 09:23:31 -0700

    correct err

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4).sort()
+    names = dir(hidden_4)
     for i in names:
         if i[0:2] != "__":
            print("{}".format(i))
