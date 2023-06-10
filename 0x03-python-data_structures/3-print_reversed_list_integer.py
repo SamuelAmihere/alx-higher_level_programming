30c4237915980fe3beb40c34c182e13966576c77
SamuelAmihere <amisam2000@gmail.com>
2023-06-10 11:36:39 -0700

    use check for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_lisit_integer(my_list=[]):
     """prints reversed form of a list of integers"""
-    if len(my_list) == 0:
+    if not my_list:
         pass
     else:
         my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
