4a24018cbce9cba4c8544be88e83ba2d0667d10d
SamuelAmihere <amisam2000@gmail.com>
2023-06-11 03:49:56 -0700

    fix bug

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -47,7 +47,7 @@ int is_palindrome(listint_t **head)
 	listint_t *curr, *temp;
 	int n = 0, i = 1;
 
-	if (!head)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
 	temp = reverse_listint(head, (size_t *) &n);
