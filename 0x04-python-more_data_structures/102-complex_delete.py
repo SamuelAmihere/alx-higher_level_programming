dbab2d424804fa4d36d3a65d774c79843e3ed0e4
SamuelAmihere <amisam2000@gmail.com>
2023-06-14 15:59:31 -0700

    fix bug

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary:
+    for k, v in a_dictionary.items():
         if v == value:
             a_dictionary.pop(k, None)
