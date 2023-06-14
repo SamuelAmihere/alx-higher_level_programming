3c39b99804fcae207c1ea634c63e54a0d28eb753
SamuelAmihere <amisam2000@gmail.com>
2023-06-14 16:06:12 -0700

    create copy

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
+    copy = a_dictionary.copy()
+    for k, v in copy.items():
         if v == value:
             a_dictionary.pop(k, None)
+    return a_dictionary
