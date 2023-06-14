9cca4882893f524b4dc4b6acff3c6be18c158ba7
SamuelAmihere <amisam2000@gmail.com>
2023-06-14 03:09:06 -0700

    add 1-search_replace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    func = lambda rw: [col * col for col in rw]
-    return [func(row) for row in matrix]
+    return [[col * col for col in row] for row in matrix]
