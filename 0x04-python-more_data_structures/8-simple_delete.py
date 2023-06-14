664d0ad385c9daa53b57d24a60be262ddfab6f50
SamuelAmihere <amisam2000@gmail.com>
2023-06-14 09:00:25 -0700

    use pop

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    temp = {}
-    for k, v in a_dictionary.items():
-        if k != key:
-            temp[k] = v
-    return temp
+    a_dictionary.pop(key, None)
+    return a_dictionary
