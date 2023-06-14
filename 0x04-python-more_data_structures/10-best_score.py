aae3111902a0d14ae04315a7883be593be5b4009
SamuelAmihere <amisam2000@gmail.com>
2023-06-14 09:59:45 -0700

    handle None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,4 +4,4 @@ def best_score(a_dictionary):
         temp = list(a_dictionary.items())
         temp.sort(key=lambda kv: kv[1], reverse=True)
         return temp[0][0]
-    return a_dictionary
+    return None
