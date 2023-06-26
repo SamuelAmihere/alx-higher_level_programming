d84db3a3a77ec0cdee9ab4166c6a43b75310a82f
SamuelAmihere <amisam2000@gmail.com>
2023-06-26 15:29:39 -0700

    add 4-list_division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             try:
                 result = my_list_1[n] / i
-            except IndexError:
+            except (IndexError, UnboundLocalError):
                 print("out of range")
                 result = 0
         except ZeroDivisionError:
