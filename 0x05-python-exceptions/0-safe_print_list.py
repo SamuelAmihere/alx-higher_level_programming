e32ed2b58ef7a97f70b34ffd95848326bf3e46e7
SamuelAmihere <amisam2000@gmail.com>
2023-06-26 12:47:20 -0700

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
-    n = 0
-
-    while (n <= x):
-
-        try:
-            print(my_list[n], end="")
-            n += 1
-        except KeyError:
-            break
-    
-    if n > 0:
+    try:
+        for n, i in enumerate(my_list[0: x]):
+            print(i, end="")
         print("")
         return (n + 1)
-    else:
-        return (0)
+    except IndexError:
+        ...
