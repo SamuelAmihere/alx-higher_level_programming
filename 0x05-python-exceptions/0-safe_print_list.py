3bca6237b0bd4a566e235cd74971d79bde12d1e4
SamuelAmihere <amisam2000@gmail.com>
2023-06-26 12:54:31 -0700

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,5 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(i, end="")
         print("")
         return (n + 1)
-    except IndexError:
-        ...
+    except (IndexError, UnboundLocalError):
+        return (0)
