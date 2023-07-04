0750d867af3c7b37ab9d9ebdbbbc5e707ed7d0bd
SamuelAmihere <amisam2000@gmail.com>
2023-07-03 21:19:59 -0700

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A pyhton module that defines
+   functions for maths operations
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+
+    Args:
+        a (int): first integer
+        b (int): second integer
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int: the sum of a and b
+    """
+    if type(a) not in [float, int]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [float, int]:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
