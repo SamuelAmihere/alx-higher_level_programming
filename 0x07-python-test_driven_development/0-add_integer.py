bed1dbb514b7b66fb95ff808fec051b92ee5237d
SamuelAmihere <amisam2000@gmail.com>
2023-07-04 22:42:09 -0700

    first commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """A pyhton module that defines functions for maths operations
-   
+
    add_integer: adds two integers
 """
+
 
 def add_integer(a, b=98):
     """Adds two integers
@@ -24,7 +25,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
-    
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
