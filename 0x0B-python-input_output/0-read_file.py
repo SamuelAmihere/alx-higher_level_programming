a9ccc27ff47d87bbe7cf0c652ab8d83a00a32739
SamuelAmihere <amisam2000@gmail.com>
2023-07-11 06:18:30 -0700

    fix bug

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Reads a file to print its contents"""
         with open("filename", 'r', encoding="UTF-8") as f:
-            print(f.readline(), end="")
+            while (f.next()):
+                print(f.readline(), end="")
