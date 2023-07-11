fc8b6a10330bb782b5f28297c940d41c24ff8172
SamuelAmihere <amisam2000@gmail.com>
2023-07-11 08:54:07 -0700

    fix bug

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,6 +13,6 @@ load_from_json_file = __import__("6-load_from_json_file").\
 f = "add_item.json"
 
 if os.path.exists(f):
-    j_lists = load_from_json_file(f)
-
-save_to_json_file([arg for arg in sys.argv[1:]], f)
+    save_to_json_file(load_from_json_file(f), f)
+else:
+    save_to_json_file([arg for arg in sys.argv[1:]], f)
