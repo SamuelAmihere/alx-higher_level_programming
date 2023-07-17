8ae9dd7b3980a254bfd4dc95b5e78381615beb79
Samuel Amihere <amisam2000@gmail.com>
2023-07-17 14:46:04 -0700

    add test_square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -20,10 +20,10 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s1.width, 10)
         s2 = Square(2)
         self.assertEqual(s2.width, 2)
+        self.assertEqual(s2.height, 2)
         s3 = Square(8, 0, 0, 12)
         self.assertEqual(s3.width, 8)
-        s4 = Square(*[2, 4, 12, 9, 1])
-        self.assertEqual(s4.width, 2)
+        self.assertEqual(s3.height, 8)
 
         # assertRaises Test for width
         # TypeError: width must be an integer
@@ -57,3 +57,70 @@ class TestSquare(unittest.TestCase):
         self.assertRaises(TypeError, Square, *[1, [2], 3, 4])
         self.assertRaises(TypeError, Square, *[1, (2,), 3, 4])
         self.assertRaises(TypeError, Square, *[1, None, 3, 4])
+
+    def test_x(self):
+        """Test for x"""
+        # assertEquals Test for x
+        s1 = Square(10)
+        self.assertEqual(s1.x, 0)
+        s2 = Square(2, 3)
+        self.assertEqual(s2.x, 3)
+        s3 = Square(8, 0, 0, 12)
+        self.assertEqual(s3.x, 0)
+        s4 = Square(2, 4, 12, 9, 1)
+        self.assertEqual(s4.x, 4)
+
+        # assertRaises Test for x
+        # TypeError: x must be an integer
+        self.assertRaises(TypeError, Square, *[1, 2, "3", 4])
+        self.assertRaises(TypeError, Square, *[1, 2, {0}, 4])
+        self.assertRaises(TypeError, Square, *[1, 2, [3], 4])
+        self.assertRaises(TypeError, Square, *[1, 2, (3,), 4])
+        self.assertRaises(TypeError, Square, *[1, 2, None, 4])
+        # ValueError: x must be >= 0
+        self.assertRaises(ValueError, Square, *[1, 2, -1, 4])
+
+    def test_y(self):
+        """Test for y"""
+        # assertEquals Test for y
+        s1 = Square(10)
+        self.assertEqual(s1.y, 0)
+        s2 = Square(2, 3, 4)
+        self.assertEqual(s2.y, 4)
+        s3 = Square(8, 0, 0, 12)
+        self.assertEqual(s3.y, 0)
+        s4 = Square(2, 4, 12, 9, 1)
+        self.assertEqual(s4.y, 12)
+
+        # assertRaises Test for y
+        # TypeError: y must be an integer
+        self.assertRaises(TypeError, Square, *[1, 2, 3, "4"])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, {4}])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, [4]])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, (4,)])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, None])
+        # ValueError: y must be >= 0
+        self.assertRaises(ValueError, Square, *[1, 2, 3, -4])
+
+    # ---------------------------Tests for id-------------------------
+    def test_id(self):
+        """Test for id"""
+        # assertEquals Test for id
+        s1 = Square(10)
+        self.assertEqual(s1.id, 1)
+        s2 = Square(2, 3)
+        self.assertEqual(s2.id, 2)
+        s3 = Square(8, 0, 0, 12)
+        self.assertEqual(s3.id, 12)
+        s4 = Square(2, 4, 12, 9, 1)
+        self.assertEqual(s4.id, 1)
+
+        # assertRaises Test for id
+        # TypeError: id must be an integer
+        self.assertRaises(TypeError, Square, *[1, 2, 3, 4.0])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, {4}])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, [4]])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, (4,)])
+        self.assertRaises(TypeError, Square, *[1, 2, 3, None])
+        # ValueError: id must be >= 0
+        self.assertRaises(ValueError, Square, *[1, 2, 3, -4])
