2d988755215adf5e9405fa1de70a13676af1f5fb
Samuel Amihere <amisam2000@gmail.com>
2023-10-18 20:34:53 -0700

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@
 SELECT name FROM tv_genres AS g
 WHERE name NOT IN
 	(SELECT name FROM tv_genres AS g
-		LEFT JOIN tv_show_genres AS tg ON g.id = tg.genre_id
-		LEFT JOIN tv_shows As s ON tg.show_id = s.id
-		WHERE s.title = "Dexter")
+	LEFT JOIN tv_show_genres AS tg ON g.id = tg.genre_id
+	LEFT JOIN tv_shows As s ON tg.show_id = s.id
+	WHERE s.title = "Dexter")
 GROUP BY name ORDER BY name ASC;
