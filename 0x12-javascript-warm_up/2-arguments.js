d81db1e81deaafd7af4c18556f2278c41e748378
Samuel Amihere <amisam2000@gmail.com>
2023-11-13 15:34:46 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const argCount = process.argv.length;
-const msg = argCount === 2 ? 'No argument': argCount === 3 ? 'Argument found': 'Arguments found';
+const msg = argCount === 2 ? 'No argument' : argCount === 3 ? 'Argument found' : 'Arguments found';
 console.log(msg);
+
