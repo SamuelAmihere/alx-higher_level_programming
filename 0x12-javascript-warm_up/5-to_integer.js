d8fecff24d5d552e0ad2a4535c9a211d36b6f70e
Samuel Amihere <amisam2000@gmail.com>
2023-11-13 16:20:29 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const argv = Number(Math.floor(process.argv[2])));
+const argv =Math.floor(Number(process.argv[2])));
 const msg = isNaN(argv) ? 'Not a number' : `My number: ${argv}`;
 console.log(msg);
