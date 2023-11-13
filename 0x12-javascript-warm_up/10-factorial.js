861ab22946ebdf3b506680ac78d9f46822d3e186
Samuel Amihere <amisam2000@gmail.com>
2023-11-13 17:17:25 +0000

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function factorial (n) {
+  const fact = isNaN(n) || n === 0 ? 1 : n * factorial(n - 1);
+}
+const result = factorial(Number(process.argv[2]));
+console.log(result);
