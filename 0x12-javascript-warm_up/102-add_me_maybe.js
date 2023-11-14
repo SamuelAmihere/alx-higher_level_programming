b8c9dbbe556bf32177028e9d9bcd6f3dad69d77b
Samuel Amihere <amisam2000@gmail.com>
2023-11-14 12:40:27 +0800

    Add me maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (x, theFunction) {
-  x = ++x
-  theFunction(x);
+exports.addMeMaybe = function (number, theFunction) {
+  number = ++number;
+  theFunction(number);
 };
