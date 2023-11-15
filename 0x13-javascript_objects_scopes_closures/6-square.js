a75cd429671c784713ff854dd5492b704443703c
Samuel Amihere <amisam2000@gmail.com>
2023-11-15 10:12:03 +0800

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
-  charPrint(c) {
+  charPrint (c) {
     if (c != undefined) {
       for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
+	console.log(c.repeat(this.width));
       }
     } else {
-        this.print();
+      this.print();
     }
   }
 };
