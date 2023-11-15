68f548bcb80640d63d6dd768fb96dbf64b8d1a36
Samuel Amihere <amisam2000@gmail.com>
2023-11-15 06:33:30 +0000

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,7 @@
-#!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let dictNew = {};
-for (let key in dict) {
-  var value = dictNew[dict[key]];
-  if (value === undefined) {
-    value = [key];
-  } else {
-    value.push(key);
-  }
-  dictNew[dict[key]] = value;
-}
-console.log(dictNew);
+t { dict } = require('./101-data');
+const converted = Object.entries(dict);
+const newDict = {};
+converted.forEach(item => {
+  newDict[item[1]] ? newDict[item[1]].push(item[0]) : newDict[item[1]] = [item[0]];
+});
+console.log(newDict);
