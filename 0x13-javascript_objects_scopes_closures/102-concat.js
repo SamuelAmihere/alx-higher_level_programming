f442b785bef8e06f8640f429e38567664bd28aa7
Samuel Amihere <amisam2000@gmail.com>
2023-11-15 14:08:54 +0800

    sorted

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const args = process.argv;
-const src_a = fs.readFileSync(args[2], 'utf8');
-const src_b = fs.readFileSync(args[3], 'utf8');
-fs.writeFileSync(args[4], src_a + src_b);
+const srca = fs.readFileSync(process.argv[2], 'utf8');
+const srcb = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], srca + srcb);
