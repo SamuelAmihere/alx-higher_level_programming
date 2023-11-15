72d641c2455f583d934496e3f641e58f171d1d39
Samuel Amihere <amisam2000@gmail.com>
2023-11-15 14:19:45 +0800

    map

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const { dict } = require('./101-data');
-const converted = Object.entries(dict);
-const newDict = {};
-converted.forEach(item => {
-  newDict[item[1]] ? newDict[item[1]].push(item[0]) : newDict[item[1]] = [item[0]];
-});
-console.log(newDict);
