f097ce5b19ef10907a013754698811888c2bf99e
Samuel Amihere <amisam2000@gmail.com>
2023-11-18 08:25:07 +0000

    chmod

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,9 +6,11 @@ import MySQLdb
 
 if __name__ == "__main__":
     args = sys.argv
+    state = args[4]
     db = MySQLdb.connect(args[1], passwd=args[2], db=args[3])
     exe = db.cursor()
-    exe.execute(f"SELECT * FROM `states` WHERE BINARY `name` = {args[4]}")
+    exe.execute("SELECT * FROM `states` \
+	WHERE BINARY `name` = {}".format(state))
 
     for state in exe.fetchall():
         print(state)
