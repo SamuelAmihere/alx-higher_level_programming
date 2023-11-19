fac3eed449f70fb0a4a400a9414ebeb9e31555de
Samuel Amihere <amisam2000@gmail.com>
2023-11-19 15:42:32 +0000

    update

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,7 +2,7 @@
 """Defines a State model from hbtn_0e_6_usa db.
 """
 
-m sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from relationship_city import Base, City
