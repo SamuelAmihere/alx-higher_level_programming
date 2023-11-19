ea4414288b855fd191cb0dd4b46b0c6df0722c50
Samuel Amihere <amisam2000@gmail.com>
2023-11-19 12:40:39 +0000

    update

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ii#!/usr/bin/python3
 """Script lists all state objects from hbtn_0e_6_usa db."""
 
 from model_state import State
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     session = sessionmaker(binddddd=engine)
     results = session.query(State).order_by(State.id)
     for state in results:
-        print("{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
