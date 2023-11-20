d30f58142a956a0fa6b9b7752e3088e9dd9998b4
Samuel Amihere <amisam2000@gmail.com>
2023-11-20 05:10:05 +0000

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Deletes all State objects with a name containing 'a' \
-    from hbtn_0e_6_usa db.
+   from hbtn_0e_6_usa db.
 """
 
 import sys
-from model_state import State
 from sqlalchemy import create_engine
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
 
