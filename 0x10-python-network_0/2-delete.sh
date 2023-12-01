802a8b60f18a8cd500d3e0f7bdbe958a13597d8e
Samuel Amihere <amisam2000@gmail.com>
2023-12-01 12:22:23 +0800

    Python networking

---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# sends a DELETE request to the URL
+
+curl -sX DELETE "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL and displays all HTTP methods
+
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
