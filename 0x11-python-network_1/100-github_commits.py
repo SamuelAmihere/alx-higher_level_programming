cde28511b401e408be741d27842d8148d2bf5898
Samuel Amihere <amisam2000@gmail.com>
2023-12-02 12:43:28 +0800

    python-network_1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list 10 commits
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+              argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for c in range(10):
+            sha = commits[c].get("sha")
+            name = commits[c].get("commit").get("author").get("name")
+            print("{}: {}".format(commits[c].get(sha, name)))
+    except IndexError:
+        ...
