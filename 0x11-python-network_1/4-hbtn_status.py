88cee1239ed287a85152487771260a7854e1a0a0
Samuel Amihere <amisam2000@gmail.com>
2023-12-01 21:16:40 -0800

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,4 @@ if __name__ == '__main__':
     url = get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(url.text)))
-    dec = url.content.decode('utf-8')
-    print("\t- content: {}".format(dec))
+    print("\t- content: {}".format(url.text))
