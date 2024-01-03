a6821955ef65bb9e3453c0e672da7576b00d7183
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 23:49:44 +0800

    Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-"""reads and prints the content of a file"""
 const fs = require('fs');
-fs.readFile(process.argv[2], "utf8", function (err, fcontent){
-	console.log(err || fcontent);
+fs.readFile(process.argv[2], 'utf8', function (err, fcontent) {
+  console.log(err || fcontent);
 });
