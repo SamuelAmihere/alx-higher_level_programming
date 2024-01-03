42789e82ad78771045e5e1f2da0866f10a948f9d
Samuel Amihere <amisam2000@gmail.com>
2024-01-03 23:54:44 +0800

    Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const req = require('request');
-req.get(process.argv[2]).on('response', function (res) {
+req.get(process.argv[2]).on('res', function (res) {
   console.log(`code: ${res.statusCode}`);
 });
