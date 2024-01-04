d8bdc3e88c1c96e6386d33b537aed7875dbdaa49
Samuel Amihere <amisam2000@gmail.com>
2024-01-04 13:44:04 +0800

    update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,15 @@
 #!/usr/bin/node
+const episode = process.argv[2];
 const req = require('request');
-req.get(process.argv[2]).on('response', res => {
-	  console.log(`code: ${res.statusCode}`);
-});
+const URL = 'https://swapi-api.hbtn.io/api/films/';
 
+req(URL + episode, (err, res, body) => {
+	  if (err) {
+		      console.log(err);
+		    } else if (res.statusCode === 200) {
+			        const JSONres = JSON.parse(body);
+			        console.log(JSONres.title);
+			      } else {
+				          console.log('Error code: ' + res.statusCode);
+				        }
+});
