fa581f0db2e75d62fd4d5581808559740ccb5970
Samuel Amihere <amisam2000@gmail.com>
2024-01-05 00:17:07 +0800

    update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const req = require('request');
-const url = process.argv[2])
-const dest = process.argv[3]
-request(url).pipe(fs.createWriteStream(dest));
+const url = process.argv[2];
+const dest = process.argv[3];
+req(url).pipe(fs.createWriteStream(dest));
