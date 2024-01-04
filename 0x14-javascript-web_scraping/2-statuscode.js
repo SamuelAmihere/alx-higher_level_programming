4466bf41103eafb6e0a041d7b3db7e0930061d7d
Samuel Amihere <amisam2000@gmail.com>
2024-01-04 13:03:54 +0800

    api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const req = require('request');
-req.get(process.argv[2]).on('response', function (res) {
-  console.log(`code: ${res.statusCode}`);
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+	  console.log(`code: ${response.statusCode}`);
 });
