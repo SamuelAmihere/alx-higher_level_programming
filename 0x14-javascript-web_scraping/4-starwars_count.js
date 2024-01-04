d6d20b2205adde71711be282e86c6031a6758ab6
Samuel Amihere <amisam2000@gmail.com>
2024-01-05 00:09:18 +0800

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
+const req = require('request');
+const id = process.argv[2];
+req(id, function (err, res, body) {
+  if (!err) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
