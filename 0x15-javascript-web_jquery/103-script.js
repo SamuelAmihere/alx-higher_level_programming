cf866e73ecf1363b79b194d0fcb54e7bca9b1e68
Samuel Amihere <amisam2000@gmail.com>
2024-01-11 13:59:58 +0800

    first

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -2,7 +2,7 @@ $('document').ready(function () {
   $('INPUT#btn_translate').click(translate);
   $('INPUT#language_code').focus(function () {
     $(this).keydown(function (e) {
-      if (e.keyCode === 13) {
+	    if (e.keyCode === 13) {
         translate();
       }
     });
@@ -12,6 +12,6 @@ $('document').ready(function () {
 function translate () {
   const url = 'https://www.fourtonfish.com/hellosalut/?';
   $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-    $('DIV#hello').html(data.hello);
+	  $('DIV#hello').html(data.hello);
   });
 }
